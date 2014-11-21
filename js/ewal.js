6ede85aab23cbd09c6c55058aaf2e8e5e0a03706
yangliume <youngleo.yzu@gmail.com>
2014-11-21 12:57:40 +0100

    remove hexo blog and redirect to my resume
    blog immigrated to farbox

--- js/ewal.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-    $('img.caption').captionjs();
-
-    $('img').each(function(){
-    	var $this = $(this);
-    	$this.wrap('<a href="' + $this.attr('src') + '"></a>');
-	});
-});
-
